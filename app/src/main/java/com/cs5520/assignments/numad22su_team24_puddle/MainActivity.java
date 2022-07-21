0892fa528f23973f370a0d579a542b4d538f544a
Nisarg Patel <patel321nisarg@gmail.com>
2022-07-21 00:30:04 -0400

    Project Puddle initialization

---
+++ app/src/main/java/com/cs5520/assignments/numad22su_team24_puddle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cs5520.assignments.numad22su_team24_puddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
