8193bdb3e0a042cd407650f75331c62e43cb73bb
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-07-27 14:12:33 -0700

    integrated header of navigation drawer, now it displays username and email by making api call

--- app/src/main/java/com/cs5520/assignments/numad22su_team24_puddle/PuddleListActivity.java
+++ app/src/main/java/com/cs5520/assignments/numad22su_team24_puddle/PuddleListActivity.java
@@ -1,5 +1,6 @@
 package com.cs5520.assignments.numad22su_team24_puddle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -9,10 +10,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.cs5520.assignments.numad22su_team24_puddle.Model.User;
+import com.cs5520.assignments.numad22su_team24_puddle.Utils.FirebaseDB;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class PuddleListActivity extends AppCompatActivity {
@@ -21,6 +33,15 @@ public class PuddleListActivity extends AppCompatActivity {
     RecyclerView puddleListRecyclerView;
     PuddleListAdapter puddleListAdapter;
     DrawerLayout drawer;
+    NavigationView navView;
+    TextView user_email;
+    TextView user_name;
+
+    // Firebase
+    FirebaseUser current_user;
+    DatabaseReference userRef;
+
+    private HashMap<String, String> userDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +49,19 @@ public class PuddleListActivity extends AppCompatActivity {
 //        setContentView(R.layout.activity_puddle_list);
         // Changed to incorporate navigation drawer activity
         setContentView(R.layout.nav_activity_main);
+        userDetails = new HashMap<>();
 
+        // Api Calls
+        fetchCurrentUserData();
+
+        // Initializing Widgets
         puddleListRecyclerView = findViewById(R.id.puddle_list_rv);
         drawer = findViewById(R.id.drawer_layout);
+        navView = findViewById(R.id.nav_view);
+        user_email = navView.getHeaderView(0).findViewById(R.id.user_email);
+        user_name = navView.getHeaderView(0).findViewById(R.id.username);
+
+        // Initializing RecyclerView
         puddleListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         puddleListAdapter = new PuddleListAdapter(this, getPuddleList());
         puddleListRecyclerView.setAdapter(puddleListAdapter);
@@ -55,8 +86,30 @@ public class PuddleListActivity extends AppCompatActivity {
     }
 
     public void clickProfile(View view){
-//        nav_view
-//        drawer.openDrawer(Gravity.LEFT);
         drawer.openDrawer(GravityCompat.START);
+    }
+
+    public void fetchCurrentUserData(){
+        current_user = FirebaseDB.getCurrentUser();
+
+        userRef = FirebaseDB.getDataReference(getString(R.string.users)).child(current_user.getUid());
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot snap: snapshot.getChildren()){
+                    userDetails.put(snap.getKey(), snap.getValue(String.class));
+                }
+
+                user_email.setText(current_user.getEmail());
+                user_name.setText(userDetails.get(getString(R.string.user_name)));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 }
